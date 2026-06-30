fe69d3baef36a126fd300b08e08333f18f5857e8
jakobkrammer26 <jakobkrammer26@gmail.com>
2026-06-30 16:12:42 +0000

    Update

--- main.js
+++
@@ -1,6 +0,0 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-createRoot(document.getElementById('root')).render(React.createElement(StrictMode, null,
-    React.createElement(App, null)));
